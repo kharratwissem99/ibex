f34328a7043dc04af5b6b7fad98cf3f5eaa021bf
Wissem <w.kharrat@stud.uni-hannover.de>
2025-09-29 12:34:12 +0200

    update toolchain path for compiler in common.mk

--- examples/sw/simple_system/common/common.mk
+++ examples/sw/simple_system/common/common.mk
@@ -19,7 +19,7 @@ SRCS = $(COMMON_SRCS) $(PROGRAM_C) $(EXTRA_SRCS)
 C_SRCS = $(filter %.c, $(SRCS))
 ASM_SRCS = $(filter %.S, $(SRCS))
 
-CC = riscv32-unknown-elf-gcc
+CC = ~/Downloads/lowrisc-toolchain-gcc-rv32imcb-x86_64-20250710-1/bin/riscv32-unknown-elf-gcc
 
 CROSS_COMPILE = $(patsubst %-gcc,%-,$(CC))
 OBJCOPY ?= $(CROSS_COMPILE)objcopy
