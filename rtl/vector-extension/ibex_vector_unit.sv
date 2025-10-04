e9e5771cd69f9aacf5b7c2ffffd1911af22c3e6c
Wissem <w.kharrat@stud.uni-hannover.de>
2025-10-04 10:21:59 +0200

    remove commented-out code for v_req_ready logic in ibex_vector_unit

--- rtl/vector-extension/ibex_vector_unit.sv
+++ rtl/vector-extension/ibex_vector_unit.sv
@@ -292,34 +292,4 @@ module ibex_vector_unit #(
     end
   end
 
-
-  // logic v_req_ready, v_req_ready_next;
-  // // Phase-1 bring-up defaults
-  // assign v_req_ready_o   = v_req_ready; // accept when IDLE (we’ll refine)
-  // assign v_resp_o        = '{done:1'b0, trap:1'b0, rd_we:1'b0, rd_wdata:32'd0, cause:5'd0};
-  // // assign mem_req_valid_o = 1'b0;
-  // // assign mem_req_addr_o  = '0;
-  // // assign mem_req_write_o = 1'b0;
-  // // assign mem_req_wdata_o = '0;
-  // // assign mem_req_wstrb_o = 4'b0000;
-  // always_ff @(posedge clk_i or negedge rst_ni) begin
-  //   if (!rst_ni) begin
-  //     // reset state
-  //     v_req_ready <= 1'b0;
-  //   end else begin
-  //     // state transitions
-  //     v_req_ready <= v_req_ready_next; 
-  //   end
-  // end
-
-  // always_comb begin
-  //   // combinational logic
-  //   v_req_ready_next = v_req_ready;
-  //   if (v_req_valid_i) begin
-  //     v_req_ready_next = 1'b1;
-  //   end
-  //   else begin
-  //     v_req_ready_next = 1'b0;
-  //   end
-  // end
 endmodule
