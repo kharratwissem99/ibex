3ee6c2814ed7796e959061f531fd8fbea7ef037c
Wissem <w.kharrat@stud.uni-hannover.de>
2025-10-07 18:21:26 +0200

    wip

--- rtl/vector-extension/ibex_vector_unit.sv
+++ rtl/vector-extension/ibex_vector_unit.sv
@@ -293,24 +293,44 @@ module ibex_vector_unit #(
   logic [1:0]  sh;                    // byte offset within 32b word
   logic        misaligned;
 
-  assign addr       = base_q + 2 * idx_q; // current byte address after incremention // 
+  localparam int BYTES_PER_BEAT = 4;
+  logic [1:0] EEW_BYTES = (sew_q==2'd0) ? 1 : 2;
+  logic [2:0] ELEMS_PER_BEAT = BYTES_PER_BEAT / EEW_BYTES; // 4 oder 2
+
+  // todo: why should we write again, if we take the bytes that we need from the first beat. Use the following to integrate this feature
+  // until now we always read two beats when misaligned
+  //logic [2:0]  Nbytes      = elems_beat * EEW_BYTES;         // 1..4 (8b: 4/3/2/1; 16b: 4/2)
+  //logic        need_second = (sh != 2'd0) && ((sh + Nbytes) > 3'd4);
+
+  logic [5:0] byte_idx;                               // bis 30 (16*2-2)
+  assign byte_idx = idx_e_q * EEW_BYTES;
+
+  assign addr       = base_q + byte_idx; // current byte address after incremention // 
   assign sh         = addr[1:0];
   assign misaligned = (sh != 2'b00);
   assign a0_aligned = {addr[31:2], 2'b00}; // aligned down
   assign a1_aligned = a0_aligned + 32'd4; // aligned up
 
   // helpers
-  logic [2:0] bytes_left       = (vl_q > idx_q) ? (vl_q - idx_q) : 3'd0;
-  logic [2:0] bytes_from_read  = (bytes_left > 3'd4) ? 3'd4 : bytes_left; // 1..4 (last can be 1..3)
+  logic [2:0] elements_left       = (vl_q > idx_q) ? (vl_q - idx_q) : 3'd0;
+  logic [2:0] elements_from_read  = (elements_left > ELEMS_PER_BEAT) ? ELEMS_PER_BEAT : elements_left; // 1..4 (last can be 1..3)
 
-  // mask for tail (idx_mod is always 0 here)
-  logic [3:0] mask =
-    (bytes_from_read == 3'd4) ? 4'b1111 :
-    (bytes_from_read == 3'd3) ? 4'b0111 :
-    (bytes_from_read == 3'd2) ? 4'b0011 :
-    (bytes_from_read == 3'd1) ? 4'b0001 : 4'b0000;
+  logic [3:0] mask;
+  // mask for tail (idx_mod is always 0 here) // todo: check if this is still correct for SEW=16
+  if (EEW_BYTES == 1) begin // sew=8
+    mask =
+      (elements_from_read == 3'd4) ? 4'b1111 :
+      (elements_from_read == 3'd3) ? 4'b0111 :
+      (elements_from_read == 3'd2) ? 4'b0011 :
+      (elements_from_read == 3'd1) ? 4'b0001 : 4'b0000;
+  end
+  else if (EEW_BYTES == 2) begin // sew=16
+    mask =
+      (elements_from_read == 3'd2) ? 4'b1111 :
+      (elements_from_read == 3'd1) ? 4'b1100 : 4'b0000;
+  end
 
-  logic [63:0] merged = {beat2_q, beat1_q};
+  logic [63:0] merged = {beat2_q, beat1_q}; // todo: check if this is still correct for SEW=16
 
   always_comb begin
     // defaults
@@ -337,18 +357,6 @@ module ibex_vector_unit #(
     // directly assigned see the assign statements above
     vrf_wr_vreg  = vd_idx_q;
     vrf_wr_bank  = idx_q[3:2];
-
-    // helpers
-    // logic [2:0] bytes_left       = (vl_q > idx_q) ? (vl_q - idx_q) : 3'd0;
-    // logic [2:0] bytes_from_read  = (bytes_left > 3'd4) ? 3'd4 : bytes_left; // 1..4 (last can be 1..3)
-
-    // // mask for tail (idx_mod is always 0 here)
-    // logic [3:0] mask =
-    //   (bytes_from_read == 3'd4) ? 4'b1111 :
-    //   (bytes_from_read == 3'd3) ? 4'b0111 :
-    //   (bytes_from_read == 3'd2) ? 4'b0011 :
-    //   (bytes_from_read == 3'd1) ? 4'b0001 : 4'b0000;
-
 
     unique case (lsu_q)
       LSU_IDLE:  if (lsu_start) begin
@@ -412,14 +420,14 @@ module ibex_vector_unit #(
 
       LSU_WRITE: begin
         // current VRF bank = idx_q[3:2]; idx_mod==0 -> no shift needed
-        vrf_wr_en    = (bytes_from_read != 0);
+        vrf_wr_en    = (elements_from_read != 0);
         vrf_wr_vreg  = vd_idx_q;
-        vrf_wr_bank  = idx_q[3:2];
+        vrf_wr_bank  = byte_idx[3:2];
         vrf_wr_wdata = word_q;     // already the right 4-byte window, aligned to byte 0
         vrf_wr_wstrb = mask;       // only low N bytes if tail
 
         // advance or finish
-        idx_d = idx_q + bytes_from_read;  // (3-bit -> zero-extends fine)
+        idx_d = idx_q + elements_from_read;  // (3-bit -> zero-extends fine)
         lsu_d = (idx_d >= vl_q) ? LSU_DONE : LSU_REQ1;
       end
 
