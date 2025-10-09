f423475c3aebaebd2eb900beff4bfa141a3ae61d
Wissem <w.kharrat@stud.uni-hannover.de>
2025-10-09 20:30:26 +0200

    fix luv

--- rtl/vector-extension/luv.sv
+++ rtl/vector-extension/luv.sv
@@ -5,7 +5,7 @@ module luv (
   input logic  [4:0]  vd_idx_i,    // destination vector register index
   input logic  [31:0]  base_i,      // base address (from rs1)
   input logic  [4:0]   vl_i,      // vector length (from rs1)
-  input logic  [4:0]   request_type_i,      // 8 oder 16
+  input logic  [2:0]   request_type_i,      // 8 oder 16
 
   input  logic         lu_rq,  // pulse to start LSU operation
   output logic         ld_done,   // pulse when operation done
@@ -51,28 +51,28 @@ module luv (
   logic [1:0]  sh;                    // byte offset within 32b word
 
   // helpers
-  logic [2:0] elements_left;       
-  logic [2:0] elems_beat;
+  logic [4:0] elements_left; // todo: why ist 5 bits      
+  logic [2:0] elems_beat; // todo: why ist 3 bits 
   logic [3:0] mask;
 
   localparam int BYTES_PER_BEAT = 4;
   logic [1:0] EEW_BYTES;
-  logic [2:0] ELEMS_PER_BEAT; // 4 oder 2
 
   logic [2:0]  Nbytes;
   logic        need_two;            // second write needed?
 
   always_comb begin
-    if (request_type_i == 3'b0) EEW_BYTES = 1 // bytes pro element
-    else if (request_type_i == 3'b101) EEW_BYTES = 2
+    if (request_type_i == 3'd0) EEW_BYTES = 2'd1; // bytes pro element
+    else if (request_type_i == 3'd5) EEW_BYTES = 2'd2;
+    else EEW_BYTES = 2'd1; // default
   end
   assign byte_idx = idx_e_q * EEW_BYTES;
   assign addr       = base_i + byte_idx; // current byte address after incremention // 
   assign a0_aligned = {addr[31:2], 2'b00}; // aligned down
 
   assign ELEMS_PER_BEAT = BYTES_PER_BEAT / EEW_BYTES;
-  assign elements_left       = (vl_q > idx_e_q) ? (vl_q - idx_e_q) : 3'd0;
-  assign elems_beat  = (elements_left > ELEMS_PER_BEAT) ? ELEMS_PER_BEAT : elements_left;
+  assign elements_left = (vl_i > idx_e_q) ? (vl_i - idx_e_q) : 5'd0;
+  assign elems_beat  = (elements_left > ELEMS_PER_BEAT) ? ELEMS_PER_BEAT : elements_left[2:0];
   assign Nbytes      = elems_beat * EEW_BYTES;
   assign sh         = addr[1:0];
   assign need_two   = (sh != 2'd0) && ((sh + Nbytes) > 3'd4); // wir brauchen zwei beats wenn die verschiebung and der anzahl der gebleiebenen Bytes größer als 4 ist.
@@ -119,8 +119,8 @@ module luv (
     data_be_o = 4'b0000;
 
     unique case (ld_state_q)
-      LSU_IDLE:  if (ld_rq) begin
-        if (vl_q == 0) ld_state_d = LSU_DONE;
+      LSU_IDLE:  if (lu_rq) begin
+        if (vl_i == 0) ld_state_d = LSU_DONE;
         else ld_state_d = LSU_REQ1;
         idx_e_d = '0; // wichtig
         word_d  = '0;
@@ -139,7 +139,7 @@ module luv (
           if (data_err_i) ld_state_d = LSU_FAULT;
           else begin
             word_d = data_rdata_i >> (8*sh);
-            ld_state_d   = need_two ? LSU_REQ2 : LSU_ALIGN;
+            ld_state_d = need_two ? LSU_REQ2 : LSU_WRITE;
           end
         end
       end
@@ -155,8 +155,8 @@ module luv (
         if (data_rvalid_i) begin
           if (data_err_i) ld_state_d = LSU_FAULT;
           else begin
-            word_d = (data_rdata_i << (4-sh)) | word_q;
-            ld_state_d   = LSU_ALIGN;
+            word_d = (data_rdata_i << (8*(4-sh))) | word_q;
+            ld_state_d = LSU_WRITE;
           end
         end
       end
@@ -169,10 +169,9 @@ module luv (
         vrf_wr_wdata = word_q;     // already the right 4-byte window, aligned to byte 0
         vrf_wr_wstrb = mask;       // only low N bytes if tail
 
-        //todo: implementiere error signal
         // advance or finish
         idx_e_d = idx_e_q + elems_beat;  // (3-bit -> zero-extends fine)
-        ld_state_d = (idx_e_d >= vl_q) ? LSU_DONE : LSU_REQ1;
+        ld_state_d = (idx_e_d >= vl_i) ? LSU_DONE : LSU_REQ1;
       end
 
       LSU_FAULT: begin
