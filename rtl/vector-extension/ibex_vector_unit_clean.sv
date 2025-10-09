321d418f6c686c6af4133440a6a1431677849762
Wissem <w.kharrat@stud.uni-hannover.de>
2025-10-09 21:25:43 +0200

    Enhance ibex_vector_unit: add LUV and SUV instantiation, update internal signals for load/store operations

--- rtl/vector-extension/ibex_vector_unit_clean.sv
+++ rtl/vector-extension/ibex_vector_unit_clean.sv
@@ -54,6 +54,7 @@ module ibex_vector_unit #(
   logic         is_vse_q, is_vse_d;
 
   logic [31:0]  rs_data_q, rs_data_d;
+  logic [6:0]   opcode_q, opcode_d;   // Added missing opcode signals
 
   // ---- Control FSM ----
   typedef enum logic [2:0] {IDLE, DECODE, EXECUTE, WRITEBK, DONE, TRAP} vstate_e;
@@ -68,9 +69,9 @@ module ibex_vector_unit #(
   logic [4:0] max_elems;
 
   // internal signals used for communication between the main control FSM and Load FSM
-  logic ld_rq, ld_done, ld_fault;
+  logic ld_rq_q, ld_rq_d, ld_done, ld_fault;
   // internal signals used for communication between the main control FSM and Store FSM
-  logic st_rq, st_done, st_fault;
+  logic st_rq_q, st_rq_d, st_done, st_fault;
 
   logic lsu_done;
   assign lsu_done = ld_done || st_done;
@@ -78,8 +79,8 @@ module ibex_vector_unit #(
   logic lsu_fault;
   assign lsu_fault = st_fault || ld_fault;
 
-  assign ld_rq = (ld_q == LSU_IDLE) && (state_q == EXECUTE) && (opcode_q == 7'h07);
-  assign st_rq = (st_q == ST_IDLE) && (state_q == EXECUTE) && (opcode_q == 7'h27);
+  // assign ld_rq = ld_idle && (state_q == EXECUTE) && (opcode_q == 7'h07);
+  // assign st_rq = st_idle && (state_q == EXECUTE) && (opcode_q == 7'h27);
 
   // todo: should we combine all sequential processes into one always_ff block? until now 3 were used.
   always_ff @(posedge clk_i or negedge rst_ni) begin
@@ -93,6 +94,9 @@ module ibex_vector_unit #(
       // reset CSR regs
       vl_q          <= '0;
       sew_q         <= '0;
+
+      ld_rq_d <= '0;
+      st_rq_d <= '0;
     end
     else  begin       
       state_q <= state_d;
@@ -104,6 +108,9 @@ module ibex_vector_unit #(
       // update CSR regs
       vl_q         <= vl_d;
       sew_q        <= sew_d;
+
+      ld_rq_q        <= ld_rq_d;
+      st_rq_q        <= st_rq_d;
     end
   end
 
@@ -144,12 +151,22 @@ module ibex_vector_unit #(
     vl_d        = vl_q;
     sew_d       = sew_q;
 
+    ld_rq_d = ld_rq_q;
+    ld_st_d = ld_st_q;
+
     case (state_q)
       IDLE: begin
         v_req_ready_o = 1'b1; // ready to take new request
         if (v_req_valid_i) begin
           if (v_req_i.insn[6:0] == 7'h57) state_d = DECODE;
-          else if (v_req_i.insn[6:0] == 7'h07 || v_req_i.insn[6:0] == 7'h07) state_d = EXECUTE;
+          else if (v_req_i.insn[6:0] == 7'h07) begin 
+            state_d = EXECUTE;
+            ld_rq_d = 1'b1;
+          end
+          else if (v_req_i.insn[6:0] == 7'h27) begin 
+            state_d = EXECUTE;
+            st_rq_d = 1'b1;
+          end
           else state_d = TRAP;
         end
       end
@@ -166,6 +183,8 @@ module ibex_vector_unit #(
       end
 
       EXECUTE: begin
+        ld_rq_d = 1'b0;
+        st_rq_d = 1'b0;
         if (opcode_q == 7'h57) begin
           v_req_ready_o = 1'b0;
           avl    = rs_data_q; // Hilfvariable
@@ -232,14 +251,107 @@ module ibex_vector_unit #(
     .wr_bank_i (vrf_wr_bank),
     .wr_wdata_i(vrf_wr_wdata),
     .wr_wstrb_i(vrf_wr_wstrb),
-    .rd_en_i   (1'b0),
-    .rd_vreg_i ('0),
-    .rd_bank_i ('0),
-    .rd_rdata_o()
+    .rd_en_i   (vrf_rd_en),
+    .rd_vreg_i (vrf_rd_vreg),
+    .rd_bank_i (vrf_rd_bank),
+    .rd_rdata_o(vrf_rd_rdata)
   `ifdef VERIF_VRF_PEEK
   ,.vrf_peek_o (vrf_peek_bus),
     .vrf_peek_idx_i (vd_idx_q)
   `endif
+  );
+
+  // ---- LUV (Load Unit Vector) Instantiation ----
+  
+  // LUV memory interface signals
+  logic        luv_data_req;
+  logic [31:0] luv_data_addr;
+  logic        luv_data_we;
+  logic [3:0]  luv_data_be;
+  logic [31:0] luv_data_wdata;
+  
+  // SUV memory interface signals  
+  logic        suv_data_req;
+  logic [31:0] suv_data_addr;
+  logic        suv_data_we;
+  logic [3:0]  suv_data_be;
+  logic [31:0] suv_data_wdata;
+  
+  // Memory interface multiplexing
+  // Priority: Load operations have priority over store operations
+  logic mem_luv_active;
+  assign mem_luv_active = luv_data_req; // todo: note until now load and store units will never work at the same time
+  
+  assign data_req_o   = mem_luv_active ? luv_data_req   : suv_data_req;
+  assign data_addr_o  = mem_luv_active ? luv_data_addr  : suv_data_addr;
+  assign data_we_o    = mem_luv_active ? luv_data_we    : suv_data_we;
+  assign data_be_o    = mem_luv_active ? luv_data_be    : suv_data_be;
+  assign data_wdata_o = mem_luv_active ? luv_data_wdata : suv_data_wdata;
+
+  luv u_luv (
+    .clk_i              (clk_i),
+    .rst_ni             (rst_ni),
+    
+    // Control interface
+    .vd_idx_i           (req_rd_q),
+    .base_i             (req_rs1_q),  
+    .vl_i               (vl_q),
+    .request_type_i     (sew_q == 2'd0 ? 5'd0 : 5'd5), // 0=SEW8, 5=SEW16 // todo: bind them with the insn.width
+    
+    .lu_rq              (ld_rq),
+    .ld_done            (ld_done),
+    .ld_fault           (ld_fault),
+    
+    // VRF interface  
+    .vrf_wr_en_o        (vrf_wr_en),
+    .vrf_wr_vreg_o      (vrf_wr_vreg),
+    .vrf_wr_bank_o      (vrf_wr_bank), 
+    .vrf_wr_wdata_o     (vrf_wr_wdata),
+    .vrf_wr_wstrb_o     (vrf_wr_wstrb),
+    
+    // Memory interface
+    .data_req_o         (luv_data_req),
+    .data_addr_o        (luv_data_addr),
+    .data_we_o          (luv_data_we),
+    .data_be_o          (luv_data_be),
+    .data_wdata_o       (luv_data_wdata),
+    
+    .data_gnt_i         (data_gnt_i & mem_luv_active), // todo: es ist ok, both kann take gnt signal
+    .data_rvalid_i      (data_rvalid_i),
+    .data_err_i         (data_err_i),
+    .data_rdata_i       (data_rdata_i)
+  );
+
+  // ---- SUV (Store Unit Vector) Instantiation ----
+  suv u_suv (
+    .clk_i              (clk_i),
+    .rst_ni             (rst_ni),
+    
+    // Control interface
+    .vd_idx_i           (req_rd_q),  // Note: for stores, this is source register
+    .base_i             (req_rs1_q),
+    .vl_i               (vl_q),
+    .request_type_i     (sew_q == 2'd0 ? 5'd0 : 5'd5), // 0=SEW8, 5=SEW16 // todo: bind them with the insn.width
+    
+    .st_rq              (st_rq),
+    .st_done            (st_done),
+    .st_fault           (st_fault),
+    
+    // VRF interface
+    .rd_en_o            (vrf_rd_en),
+    .rd_vreg_o          (vrf_rd_vreg),
+    .rd_bank_o          (vrf_rd_bank),
+    .rd_rdata_i         (vrf_rd_rdata),
+    
+    // Memory interface
+    .data_req_o         (suv_data_req),
+    .data_addr_o        (suv_data_addr),  
+    .data_we_o          (suv_data_we),
+    .data_be_o          (suv_data_be),
+    .data_wdata_o       (suv_data_wdata),
+    
+    .data_gnt_i         (data_gnt_i & ~mem_luv_active) // todo: es ist ok, both kann take gnt signal
+    // Note: SUV doesn't need rvalid/err/rdata for simple stores
   );
 
 endmodule
