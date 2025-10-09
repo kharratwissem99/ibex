66ebbe5f72f22afd56935a9351a55b198e138e31
Wissem <w.kharrat@stud.uni-hannover.de>
2025-10-09 12:32:43 +0200

    Refactor element handling in vector unit: streamline calculations for elements left and bytes needed, and improve mask generation logic

--- rtl/vector-extension/ibex_vector_unit_clean.sv
+++ rtl/vector-extension/ibex_vector_unit_clean.sv
@@ -279,25 +279,26 @@ module ibex_vector_unit #(
   logic        misaligned;
 
   // helpers
-  logic [2:0] elements_left       = (vl_q > idx_q) ? (vl_q - idx_q) : 3'd0;
-  logic [2:0] elements_from_read  = (elements_left > ELEMS_PER_BEAT) ? ELEMS_PER_BEAT : elements_left; // 1..4 (last can be 1..3)
-
+  logic [2:0] elements_left;       
+  logic [2:0] elems_beat;
   logic [3:0] mask;
+  logic [63:0] merged; // todo: Latches/Konflikt risiko warum ??
 
   localparam int BYTES_PER_BEAT = 4;
-  logic [1:0] EEW_BYTES = (sew_q==2'd0) ? 1 : 2;
-  logic [2:0] ELEMS_PER_BEAT = BYTES_PER_BEAT / EEW_BYTES; // 4 oder 2
+  logic [1:0] EEW_BYTES;
+  logic [2:0] ELEMS_PER_BEAT; // 4 oder 2
 
-  // Use latched request values to derive base/index
+  logic [2:0]  Nbytes;
+  logic        need_two;            // second write needed?
+
   assign base_q = req_rs1_q;
   assign vd_idx_q = req_rd_q;
-
-  
-
-  // todo: why should we write again, if we take the bytes that we need from the first beat. Use the following to integrate this feature
-  // until now we always read two beats when misaligned
-  //logic [2:0]  Nbytes      = elems_beat * EEW_BYTES;         // 1..4 (8b: 4/3/2/1; 16b: 4/2)
-  //logic        need_second = (sh != 2'd0) && ((sh + Nbytes) > 3'd4);
+  assign elements_left       = (vl_q > idx_q) ? (vl_q - idx_q) : 3'd0;
+  assign elems_beat  = (elements_left > ELEMS_PER_BEAT) ? ELEMS_PER_BEAT : elements_left;
+  assign EEW_BYTES = (sew_q==2'd0) ? 1 : 2;
+  assign ELEMS_PER_BEAT = BYTES_PER_BEAT / EEW_BYTES;
+  assign Nbytes      = elems_beat * EEW_BYTES;
+  assign need_two   = (sh != 2'd0) && ((sh + Nbytes) > 3'd4); // wir brauchen zwei beats wenn die verschiebung and der anzahl der gebleiebenen Bytes größer als 4 ist.
 
   assign byte_idx = idx_e_q * EEW_BYTES;
   assign addr       = base_q + byte_idx; // current byte address after incremention // 
@@ -307,20 +308,21 @@ module ibex_vector_unit #(
   assign a1_aligned = a0_aligned + 32'd4; // aligned up
 
   // mask for tail (idx_mod is always 0 here) // todo: check if this is still correct for SEW=16
-  if (EEW_BYTES == 1) begin // sew=8
-    mask =
-      (elements_from_read == 3'd4) ? 4'b1111 :
-      (elements_from_read == 3'd3) ? 4'b0111 :
-      (elements_from_read == 3'd2) ? 4'b0011 :
-      (elements_from_read == 3'd1) ? 4'b0001 : 4'b0000;
+  
+  always_comb begin 
+    if (EEW_BYTES == 1) begin // sew=8
+      mask =
+        (elems_beat == 3'd4) ? 4'b1111 :
+        (elems_beat == 3'd3) ? 4'b0111 :
+        (elems_beat == 3'd2) ? 4'b0011 :
+        (elems_beat == 3'd1) ? 4'b0001 : 4'b0000;
+    end
+    else if (EEW_BYTES == 2) begin // sew=16
+      mask =
+        (elems_beat == 3'd2) ? 4'b1111 :
+        (elems_beat == 3'd1) ? 4'b1100 : 4'b0000;
+    end
   end
-  else if (EEW_BYTES == 2) begin // sew=16
-    mask =
-      (elements_from_read == 3'd2) ? 4'b1111 :
-      (elements_from_read == 3'd1) ? 4'b1100 : 4'b0000;
-  end
-
-  logic [63:0] merged; // todo: Latches/Konflikt risiko warum ??
 
   always_comb begin
     // defaults
@@ -424,14 +426,14 @@ module ibex_vector_unit #(
 
       LSU_WRITE: begin
         // current VRF bank = idx_q[3:2]; idx_mod==0 -> no shift needed
-        vrf_wr_en    = (elements_from_read != 0);
+        vrf_wr_en    = (elems_beat != 0);
         vrf_wr_vreg  = vd_idx_q;
         vrf_wr_bank  = byte_idx[3:2];
         vrf_wr_wdata = word_q;     // already the right 4-byte window, aligned to byte 0
         vrf_wr_wstrb = mask;       // only low N bytes if tail
 
         // advance or finish
-        idx_d = idx_q + elements_from_read;  // (3-bit -> zero-extends fine)
+        idx_d = idx_q + elems_beat;  // (3-bit -> zero-extends fine)
         ld_d = (idx_d >= vl_q) ? LSU_DONE : LSU_REQ1;
       end
 
@@ -465,30 +467,13 @@ module ibex_vector_unit #(
     end
   end
 
-  //logic [31:0] base_q;              // req_rs1_q (latched earlier)
-  //logic  [4:0] vs_idx_q;            // source vreg index (use rd field in your proxy scheme)
-  //logic  [4:0] idx_q, idx_d;        // byte element index 0..15
-  //todo: many signals were redefined here, should be shared with load LSU
   logic [31:0] bank_word_q, bank_word_d;  // 32-bit from VRF bank
   logic [31:0] w0_data, w1_data;    // store beats' data
   logic  [3:0] w0_strb, w1_strb;    // store beats' byte-enables
-  //logic [31:0] a0_aligned, a1_aligned;
-  //logic [31:0] addr;                // base_q + idx_q
-  //logic [1:0]  sh;                  // addr[1:0]
-  //logic [2:0]  bytes_left;          // vl-idx (clamped)
-  //logic [2:0]  N;                   // bytes to store this beat = min(4, bytes_left)
-  logic        need_two;            // second write needed?
 
   // VRF connections for store
   assign vrf_rd_vreg = vs_idx_q;
   assign vrf_rd_bank = byte_idx[3:2];
-
-  //assign bytes_left = (vl_q > idx_q) ? (vl_q - idx_q) : 3'd0;
-  //assign N          = (bytes_left > 3'd4) ? 3'd4 : bytes_left; // same as bytes from read
-
-  // 1 or 2 stores this beat?
-  logic [2:0]  Nbytes      = elems_beat * EEW_BYTES;         // 1..4 (8b: 4/3/2/1; 16b: 4/2)
-  assign need_two   = (sh != 2'd0) && ((sh + Nbytes) > 3'd4); // wir brauchen zwei beats wenn die verschiebung and der anzahl der gebleiebenen Bytes größer als 4 ist.
 
   always_comb begin
     st_d          = st_q;
