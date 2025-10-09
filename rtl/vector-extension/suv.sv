458235301d4e374da5212c1871b35553ccaf6c17
Wissem <w.kharrat@stud.uni-hannover.de>
2025-10-09 20:18:30 +0200

    fix suv.sv

--- rtl/vector-extension/suv.sv
+++ rtl/vector-extension/suv.sv
@@ -5,7 +5,7 @@ module suv (
   input logic  [4:0]  vd_idx_i,    // destination vector register index
   input logic  [31:0]  base_i,      // base address (from rs1)
   input logic  [4:0]   vl_i,      // vector length (from rs1)
-  input logic  [4:0]   request_type_i,      // 8 oder 16
+  input logic  [2:0]   request_type_i,      // 8 oder 16
 
   input  logic         st_rq,
   output logic         st_done,
@@ -59,11 +59,12 @@ module suv (
   logic [31:0] bank_word_q, bank_word_d;  // 32-bit from VRF bank
   logic [3:0]  w0_strb_l, w1_strb_l;
   logic [31:0] w0_data_l, w1_data_l;
-  logic [2:0]  first, socond;
+  logic [2:0]  first, second;
 
   always_comb begin
-    if (request_type_i == 3'b0) EEW_BYTES = 1 // bytes pro element
+    if (request_type_i == 3'b000) EEW_BYTES = 1 // bytes pro element
     else if (request_type_i == 3'b101) EEW_BYTES = 2
+    else EEW_BYTES = 1
   end
   assign byte_idx = idx_e_q * EEW_BYTES;
   // VRF connections for store
@@ -81,7 +82,7 @@ module suv (
   assign need_two   = (sh != 2'd0) && ((sh + Nbytes) > 3'd4); // wir brauchen zwei beats wenn die verschiebung and der anzahl der gebleiebenen Bytes größer als 4 ist.
 
   assign first = need_two ? (3'd4 - {1'b0,sh}) : Nbytes;
-  assign second= need_two ? (Nbytes - first)         : 3'd0;
+  assign second = need_two ? (Nbytes - first) : 3'd0;
   // one-beat packet (or first of two)
   assign w0_data_l = bank_word_q << (8*sh);
   assign w0_strb_l = (first==3'd4) ? 4'b1111 : ((4'b1111 >> (4 - first)) << sh);
@@ -93,17 +94,24 @@ module suv (
   assign a1_aligned = a0_aligned + 32'd4; // aligned up
 
   // VRF connections for store
-  assign vrf_rd_vreg_o = vd_idx;
-  assign vrf_rd_bank_o = byte_idx[3:2];
+  logic        vrf_rd_en;
+  logic  [4:0] vrf_rd_vreg;  
+  logic  [1:0] vrf_rd_bank;
+  
+  assign rd_en_o   = vrf_rd_en;
+  assign rd_vreg_o = vrf_rd_vreg;
+  assign rd_bank_o = vrf_rd_bank;
+  assign vrf_rd_vreg = vd_idx_i;
+  assign vrf_rd_bank = byte_idx[3:2];
 
   always_comb begin
-    st_state_d          = st_state_q;
+    st_state_d    = st_state_q;
 
     // status
     st_done       = 1'b0;
     st_fault      = 1'b0;
 
-    vrf_rd_en     = 1'b0;
+    vrf_rd_en = 1'b0;
 
     idx_e_d         = idx_e_q;
     bank_word_d   = bank_word_q;
@@ -118,16 +126,16 @@ module suv (
     unique case (st_state_q)
       ST_IDLE: begin
         if (st_rq) begin
-          if (vl_i == 0) st_state_d  = ST_VRF_RD;
-          else st_state_d  = ST_DONE;
+          if (vl_i == 0) st_state_d = ST_DONE;
+          else st_state_d = ST_VRF_RD;
           idx_e_d = '0; // wichtig
         end
       end
   
       ST_VRF_RD: begin
-        vrf_rd_en   = 1'b1;
+        vrf_rd_en = 1'b1;
         bank_word_d = rd_rdata_i;
-        st_state_d        = ST_REQ1;
+        st_state_d = ST_REQ1;
       end
 
       // first memory write
@@ -141,12 +149,8 @@ module suv (
       end
 
       ST_WAIT1: begin
-        // if your memory returns store responses, wait for rvalid_i
-        if (data_rvalid_i) begin
-          if (data_err_i) st_state_d = ST_FAULT;
-          else            st_state_d = need_two ? ST_REQ2 : ST_DONE;
-        end
-        // if not, you can shortcut: st_state_d = need_two ? ST_REQ2 : ST_DONE;
+        // Simplified: assume stores don't need response
+        st_state_d = need_two ? ST_REQ2 : ST_DONE;
       end
 
       // optional second write
@@ -160,14 +164,9 @@ module suv (
       end
 
       ST_WAIT2: begin
-        if (data_rvalid_i) begin
-          if (data_err_i) st_state_d = ST_FAULT;
-          else begin           
-            //st_state_d = ST_DONE;
-            idx_e_d = idx_e_q + elems_beat;                  // zero-extends fine
-            st_state_d  = (idx_e_d >= vl_i) ? ST_DONE : ST_VRF_RD;
-          end
-        end
+        // Simplified: assume stores don't need response todo: do stores need data_rvalid_i response??
+        idx_e_d = idx_e_q + elems_beat;
+        st_state_d = (idx_e_d >= vl_i) ? ST_DONE : ST_VRF_RD;
       end
 
       ST_DONE: begin
@@ -189,8 +188,9 @@ module suv (
       idx_e_q   <= '0;
       bank_word_q  <= '0;
     end else begin
-      st_state_q   <= st_state_d;
-      idx_e_q   <= idx_e_d;
-      bank_word_q  <= bank_word_d;
+      st_state_q <= st_state_d;
+      idx_e_q <= idx_e_d;
+      bank_word_q <= bank_word_d;
     end
+  end
 endmodule
