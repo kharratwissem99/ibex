4007b8957fdb1aec4a61b0d1f66d28dc25c0388c
Wissem <w.kharrat@stud.uni-hannover.de>
2025-10-16 21:30:44 +0200

    update SRAM initialization file path for vector store test

--- examples/simple_system/rtl/ibex_simple_system.sv
+++ examples/simple_system/rtl/ibex_simple_system.sv
@@ -19,7 +19,7 @@
 `endif
 
 `ifndef SRAMInitFile
-  `define SRAMInitFile "/home/kharrat/Desktop/repos/ibex/examples/sw/simple_system/my_test/my_test.vmem"
+  `define SRAMInitFile "/home/kharrat/Desktop/repos/ibex/examples/sw/simple_system/my_test_vec_store/vector_test.vmem"
 `endif
 /**
  * Ibex simple system
