0591c81b880adc509a0684154d05916bdfab8967
Wissem <w.kharrat@stud.uni-hannover.de>
2025-11-02 12:00:37 +0100

    test1 succeeded

--- examples/sw/simple_system/test_c_vector/test_c_vector.c
+++ examples/sw/simple_system/test_c_vector/test_c_vector.c
@@ -12,13 +12,27 @@ int main(int argc, char **argv) {
   int32_t a[4] = {1, 2, 3, 4};
   int32_t b[4] = {10, 20, 30, 40};
   int32_t c[4];
+
+  int32_t expected_c32[4] = {11, 22, 33, 44};
+
   vint32m1_t va = __riscv_vle32_v_i32m1(a, vl);
   vint32m1_t vb = __riscv_vle32_v_i32m1(b, vl);
   vint32m1_t vc = __riscv_vadd_vv_i32m1(va, vb, vl);
   __riscv_vse32_v_i32m1(c, vc, vl);
 
-  for (int i = 0; i < 4; i++)
-      puthex(c[i]);
+  for (int i = 0; i < 4; i++){
+    if (expected_c32[i] != c[i]){
+        puts("failed ");
+    }
+    else{
+        puts("passed ");
+    }
+    puts("is: ");
+    puthex(c[i]);
+    puts(" soll: ");
+    puthex(expected_c32[i]);
+    putchar('\n');
+  }
   putchar('\n');
 
   return 0;
